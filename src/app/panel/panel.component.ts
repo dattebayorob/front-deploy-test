37b037adaa4cdbf121df443df3b37a2cbd874e1b
dattebayorob <robson.william65@gmail.com>
2019-02-08 17:18:57 -0300

    Mock phrases to translate, using static template for now

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Phrase } from '../shared/phrase.model';
+import { PHRASE } from './phrase-mock';
+
+
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.css']
 })
 export class PanelComponent implements OnInit {
-
-  constructor() { }
+  public title: string = 'Traduza a frase: ';
+  public phrases: Array<Phrase> = PHRASE;
+  constructor() { 
+    console.log(this.phrases)
+  }
 
   ngOnInit() {
   }
---
+++ src/app/shared/phrase.model.ts
@@ -0,0 +1,8 @@
+export class Phrase{
+    public eng: string;
+    public ptBr: string;
+    constructor(eng:string, ptBr: string){
+        this.eng = eng;
+        this.ptBr = ptBr;
+    }
+}
