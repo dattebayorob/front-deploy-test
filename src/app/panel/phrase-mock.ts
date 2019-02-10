c90a73239dff025970121622da8df7d4dfaa8caf
dattebayorob <robson.william65@gmail.com>
2019-02-09 21:31:23 -0300

    Correct translation

--- src/app/panel/phrase-mock.ts
+++ src/app/panel/phrase-mock.ts
@@ -2,7 +2,7 @@ import { Phrase } from  '../shared/phrase.model'
 
 export const PHRASE: Array<Phrase> = [
     {eng: 'This is crazy AF', ptBr: 'Isso é louco pra caralho'},
-    {eng: "I'm too old for this shit", ptBr: 'Estou velhor demais pra essa merda'},
+    {eng: "I'm too old for this shit", ptBr: 'Estou velho demais pra essa merda'},
     {eng: 'May the force be with you', ptBr: 'Que a força esteja contigo'},
     {eng: 'He who must not be named', ptBr: 'Aquele que não deve ser nomeado'}
 ] 
