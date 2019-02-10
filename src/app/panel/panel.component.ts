0447a29f548a6b78f968bc929bbdbdd20daadb9c
dattebayorob <robson.william65@gmail.com>
2019-02-09 21:00:19 -0300

    Translation check logic, added constants to define current phrase to translate and current 'life'

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -12,11 +12,38 @@ import { PHRASE } from './phrase-mock';
 export class PanelComponent implements OnInit {
   public title: string = 'Traduza a frase: ';
   public phrases: Array<Phrase> = PHRASE;
+  public response: string;
+
+  public turn: number = 0;
+  public life: number = 3;
+  public phraseCurrent: Phrase;
+
   constructor() { 
-    console.log(this.phrases)
+    this.setPhraseCurrent(this.phrases[this.turn]);
   }
 
   ngOnInit() {
+  }
+
+  public setResponse(response: string): void{
+    this.response = response;
+  }
+  public getResponse(): string{
+    return this.response;
+  }
+  public setPhraseCurrent(phraseCurrent: Phrase):void{
+    this.phraseCurrent = phraseCurrent;
+  }
+  public checkTranslation():void{
+    if(this.phraseCurrent.ptBr == this.response){
+      this.turn++;
+      this.setPhraseCurrent(this.phrases[this.turn]);
+      alert('Acertou mizeravi');
+    }else{
+      this.life--
+      alert('Errou mazela');
+    }
+    
   }
 
 }
